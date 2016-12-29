ae9cf1c49b2c8399df79fce37c77c4d70754788d
wongandydev <wongandy97@gmail.com>
2016-12-29 10:50:53 -0500

    CodeCademyVacationCompare Complete

--- codecademyVacationCompare.js
+++ codecademyVacationCompare.js
@@ -1,1 +1,13 @@
 //Codecademy vacationCompare
+//Double For Loop
+
+var myPlaces = ['Los Angeles'. 'San Francisco'. 'Hong Kong'];
+var friendPlaces = ['Las Vegas', 'Hong Kong' , 'Macau'];
+
+for (var i = 0; i < myPlaces.length; i++){
+	for (var j = 0; j < friendPlaces.length; j++){
+		if (myPlaces[i] === friendPlaces[j]){
+			console.log(friendPlaces[j]);
+		}
+	}
+}
